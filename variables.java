bf4c213003be55c66a11bd5cf905a40ad425d833
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2021-12-01 22:02:39 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,10 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class variables {
+    public static void main(String[] args) {
+        int a = 18;
+        int b = 28;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
